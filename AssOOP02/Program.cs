b80e8877ea1bcf3d4bc1bf185017e2948bcbb7ae
SI01029 <SaeedIbrahim01029@gmail.com>
2025-07-19 13:11:03 +0300

    Q1 AssOOP02

--- AssOOP02/Program.cs
+++ AssOOP02/Program.cs
@@ -4,9 +4,14 @@
     {
         static void Main(string[] args)
         {
+            #region Q1
+            //Rectangle rectangle = new Rectangle();
+            //rectangle.Height = -1;
+            //rectangle.Width = -5;
+            //rectangle.DisblayInfo();
 
-
-
+            #endregion
         }
+
     }
 }
