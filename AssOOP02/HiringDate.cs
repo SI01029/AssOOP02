7ce7247302c58b752ccd7ad335a7fdcf1cac0aa2
SI01029 <SaeedIbrahim01029@gmail.com>
2025-07-19 17:37:51 +0300

    HireDate

--- AssOOP02/HiringDate.cs
+++ AssOOP02/HiringDate.cs
@@ -6,7 +6,55 @@ using System.Threading.Tasks;
 
 namespace AssOOP02
 {
-    internal class HiringDate
+    internal class HireDate
     {
+        #region propartey
+        public int Day { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; } 
+        #endregion
+
+        #region Constructor
+        public HireDate(int day, int month, int year)
+        {
+            if (isValidate(day, month, year))
+            {
+                Day = day;
+                Month = month;
+                Year = year;
+            }
+            else
+            {
+                Console.WriteLine("Invalid Date :(");
+            }
+
+        }
+        #endregion
+
+        #region IsValidate
+        private bool isValidate(int day, int month, int year)
+        {
+            return
+            DateTime.TryParse($"{year}-{month}-{day}", out _);
+
+        }
+        #endregion
+
+        #region ToString
+        public override string ToString()
+        {
+            return
+                $"{Day:D2}/{Month:D2}/{Year}";
+
+        }
+        #endregion
+
+        #region ToDateTime
+        public DateTime ToDateTime()
+        {
+            return new DateTime(Year, Month, Day);
+        } 
+        #endregion
+
     }
 }
