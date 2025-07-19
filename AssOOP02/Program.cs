13a02732c11a7880a69a612ffed4080078dc576c
SI01029 <SaeedIbrahim01029@gmail.com>
2025-07-19 17:06:08 +0300

    Q2,3,4,5

--- AssOOP02/Program.cs
+++ AssOOP02/Program.cs
@@ -12,14 +12,34 @@
 
             #endregion
 
-            #region Q2
+            #region Q2 ,Q3 ,Q4 ,Q5
 
-            //Employee[] emp = new Employee[4];
-            //emp[0] = new Employee(5, "Doaa", Gender.male, 4_500, new DateTime(2022, 06, 8), SecurityLevel.DBA);
+            //Employee[] emp = new Employee[3];
 
-            //Console.WriteLine(emp[0]); 
-            #endregion
+            //emp[0] = new Employee(5, "Doaa", Gender.female, 4_500, new DateTime(2022, 06, 8), SecurityLevel.DBA);
+            //emp[1] = new Employee(3, "Amina", Gender.female, 5_000, new DateTime(2016, 4, 5), SecurityLevel.guest);
+            //emp[2] = new Employee(2, "saeed", Gender.male, 6_000, new DateTime(2020, 6, 7), SecurityLevel.secertary);
+
+
+            //foreach (Employee employee in emp)
+            //{
+            //    Console.WriteLine($"{employee}");
+            //    Console.WriteLine("====================================");
+
+            //}
+
+            //Array.Sort(emp, (a, b) => b.HireDate.CompareTo(a.HireDate));
+            //Console.WriteLine("Array sorted");
+            //Console.WriteLine("===================================");
+
+            //foreach (Employee employee in emp)
+            //{
+            //    Console.WriteLine($"{employee}");
+            //    Console.WriteLine("====================================");
+            //}
+                #endregion
+
+            }
+
         }
-
-    }
 }
